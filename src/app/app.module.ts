95ac490001d7dddb9ac462d65fa823d1daf6a96f
ekeilholz+git@outlook.com <ekeilholz+git@outlook.com>
2018-07-04 09:31:00 +0200

    Initial project commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { AppRoutingModule } from './app-routing.module';
+import { LandingModule } from './landing/landing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, UiModule, AppRoutingModule, LandingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
